046223d7d984ed81d97c7bc30d55ce2b363c45e7
Jun Xu <xujun@cmss.chinamobile.com>
2017-12-13 20:55:19 -0500

    Add 5 longest_palindrome

--- leetcode/5_longest_palindrome/longest_palindrome.go
+++ leetcode/5_longest_palindrome/longest_palindrome.go
@@ -1,5 +1,25 @@
 package longest_palindrome
 
 func LongestPalindrome(s string) string {
-	for i := 
+	maxP := ""
+	for i := 0; i < len(s); i++ {
+		ret1 := extendCenter(s, i, i)
+		if len(maxP) < len(ret1) {
+			maxP = ret1
+		}
+
+		ret2 := extendCenter(s, i, i+1)
+		if len(maxP) < len(ret2) {
+			maxP = ret2
+		}
+	}
+	return maxP
+}
+
+func extendCenter(s string, left, right int) string {
+	for left >=0 && right < len(s) && s[left] == s[right] {
+		left = left - 1
+		right = right + 1
+	}
+	return s[left+1 : right]
 }
